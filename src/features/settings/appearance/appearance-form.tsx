da89a95472fcfbeade2c3176174e20fcab23c2fc
0xCNY <c.eissler123@gmail.com>
2026-04-02 16:00:40 +0200

    fix: vercel

--- src/features/settings/appearance/appearance-form.tsx
+++ src/features/settings/appearance/appearance-form.tsx
@@ -12,11 +12,10 @@ import { cn } from "@/lib/utils"
 
 const appearanceFormSchema = z.object({
   theme: z.enum(["light", "dark"], {
-    required_error: "Please select a theme.",
+    error: "Please select a theme.",
   }),
   font: z.enum(["inter", "manrope", "system"], {
-    invalid_type_error: "Select a font",
-    required_error: "Please select a font.",
+    error: "Please select a font.",
   }),
 })
 
